bb5daacf06db60c6c72a9523375a3296541bd21a
Clemeleon <clem@clemeleon.com>
2020-08-17 21:42:11 +0300

    restructure

---
+++ src/Ecommerce/Config/Config.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Package: Cleon Framework.
+ * Author: Clem Dada<clem@clemeleon.com>.
+ * 2020
+ */
+
+namespace Ecommerce\Config;
+
+
+class Config
+{
+    public function __construct()
+    {
+    }
+}
--- v1/models/UserException.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types = 1);
-
-
-class UserException extends Exception {}
